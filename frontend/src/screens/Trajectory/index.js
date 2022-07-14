0f9ede281b3cb647ece344dde1885a34df8ff1b7
RCristyan <rcristyan9@gmail.com>
2022-07-14 18:25:43 -0300

    Added trajectory base screen

---
+++ frontend/src/screens/Trajectory/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+function Trajectory() {
+    return (
+        <div id="trajectory">
+            <p>
+                Hello world
+            </p>
+        </div>
+    )
+}
+
+export default Trajectory;
