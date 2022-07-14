7c32a905a5d983f1560246247da6fca36da3c6b0
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2022-07-14 02:09:23 -0300

    added test database

---
+++ backend/api/database/create_database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE irrigador_api
+	WITH
+		ENCODING = UTF8
+		LC_COLLATE = 'pt_BR.UTF-8'
+		LC_CTYPE = 'pt_BR.UTF-8'
+		TEMPLATE = template0;
+
+\c irrigador_api
+
+CREATE SCHEMA IF NOT EXISTS "public";
+
+CREATE TABLE "public"."sensores" (
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY(start 1)
+
+	CONSTRAINT "PK_sensores" PRIMARY KEY ("id")
+);
