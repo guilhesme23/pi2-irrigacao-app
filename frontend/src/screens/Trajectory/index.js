e9d4199f0e3d28b183e998069e1ba7ba04e9c852
RCristyan <rcristyan9@gmail.com>
2022-07-15 15:47:45 -0300

    Added area properties form component

--- frontend/src/screens/Trajectory/index.js
+++ frontend/src/screens/Trajectory/index.js
@@ -1,10 +1,12 @@
 import './index.css'
 import Sidebar from '../../components/Sidebar'
+import AreaPropertiesForm from '../../components/AreaPropertiesForm'
 
 function Trajectory() {
     return (
         <div id="trajectory">
             <Sidebar />
+            <AreaPropertiesForm />
         </div>
     )
 }
