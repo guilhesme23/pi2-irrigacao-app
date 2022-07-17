1b0999a2224713856f90d2c59e9674c05079aa22
RCristyan <rcristyan9@gmail.com>
2022-07-17 11:28:58 -0300

    Added area display component

---
+++ frontend/src/components/AreaDisplay/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+function AreaDisplay() {
+    return (
+        <div id="area-display">
+            <div id="grid-block">
+                <div id="top-numbers">
+                    <p>4</p>
+                    <p>3</p>
+                </div>
+                <div id="bottom-numbers">
+                    <p>1</p>
+                    <p>2</p>
+                </div>
+                <p id="sprinkler-range-text">
+                    Área de alcance do aspersor = 1m
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AreaDisplay;
--- frontend/src/screens/Trajectory/index.js
+++ frontend/src/screens/Trajectory/index.js
@@ -1,12 +1,14 @@
 import './index.css'
 import Sidebar from '../../components/Sidebar'
 import AreaPropertiesForm from '../../components/AreaPropertiesForm'
+import AreaDisplay from '../../components/AreaDisplay'
 
 function Trajectory() {
     return (
         <div id="trajectory">
             <Sidebar />
             <AreaPropertiesForm />
+            <AreaDisplay />
         </div>
     )
 }
