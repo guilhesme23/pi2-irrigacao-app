31f2c767eddc1e1fd60cd1904cd620011dcb12ba
RCristyan <rcristyan9@gmail.com>
2022-07-20 12:16:52 -0300

    Added DataBox component base

---
+++ frontend/src/components/DataBox/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+function DataBox() {
+    return(
+        <div id='data-box'>
+            <div id="battery-box"></div>
+            <div id="time-activity-box"></div>
+            <div id="sensor-status-box"></div>
+        </div>
+    )
+}
+
+export default DataBox;
