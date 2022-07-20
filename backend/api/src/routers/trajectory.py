16893115169bf318f896b88d2fc16651304acec6
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2022-07-20 11:52:23 -0300

    trajectory get with id using database simulations(list)

--- backend/api/src/routers/trajectory.py
+++ backend/api/src/routers/trajectory.py
@@ -8,11 +8,22 @@ class MapDimensions(BaseModel):
 
 router = APIRouter()
 
+database_simul = []
+
 @router.get("/trajectory/", tags = ["Trajectory"])
 def get_trajectory_data():
-	return {"message": "trajectory data"}
+	return {"message": "trajectory data", "status": "Success", "data": database_simul}
+
+
+@router.get("/trajectory/{id}", tags = ["Trajectory"])
+def get_trajectory_data_with_id(id: int):
+	return {"message": "trajectory data", "status": "Success", "data": database_simul[id]}
+
 
 @router.post("/trajectory/", tags = ["Trajectory"])
 def post_map_data(map_dimensions: MapDimensions):
-	return {"message": "Trajectory generated", "status": "Success", "data": hamiltonian_path_brute_force(gen_grid(map_dimensions.height, map_dimensions.lenght))}
+	trajectory = hamiltonian_path_brute_force(gen_grid(map_dimensions.height, map_dimensions.lenght))
+	database_simul.append(trajectory)
+
+	return {"message": "Trajectory generated", "status": "Success", "data": trajectory}
 
