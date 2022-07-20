0de1aea03f82c9cbbe708f4c50956fedf5678956
RCristyan <rcristyan9@gmail.com>
2022-07-20 11:51:48 -0300

    Added sidebar button linking

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,20 +1,21 @@
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMap, faChartPie, faFileLines } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
     return (
         <div id="sidebar">
             <div id="buttons-box">
-                <button id="trajectory-button">
+                <Link id="sidebar-button" to='/'>
                     <FontAwesomeIcon id='fa-icon' icon={faMap}/>
                     <p>Trajetória</p>
-                </button>
-                <button id="sensors-button">
+                </Link>
+                <Link id="sidebar-button" to='/data'>
                     <FontAwesomeIcon id='fa-icon' icon={faChartPie}/>
                     <p>Sensores</p>
-                </button>
-                <button id="reports-button">
+                </Link>
+                <button id="sidebar-button">
                     <FontAwesomeIcon id='fa-icon' icon={faFileLines}/>
                     <p>Relatórios</p>
                 </button>
