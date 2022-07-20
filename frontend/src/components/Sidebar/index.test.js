02f96f6859066e1431053a0bbcb837b9bcd75f9e
RCristyan <rcristyan9@gmail.com>
2022-07-20 11:58:43 -0300

    Refactor unit test to allow router

--- frontend/src/components/Sidebar/index.test.js
+++ frontend/src/components/Sidebar/index.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Sidebar from ".";
 
 test('if sidebar buttons are rendered', () => {
-    render(<Sidebar />);
+    render(
+        <MemoryRouter>
+            <Sidebar />
+        </MemoryRouter>
+    );
+
     expect(screen.getByText("Trajetória")).toBeInTheDocument();
     expect(screen.getByText("Relatórios")).toBeInTheDocument();
     expect(screen.getByText("Sensores")).toBeInTheDocument();
