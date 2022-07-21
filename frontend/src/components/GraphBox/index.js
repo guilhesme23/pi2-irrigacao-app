f7eb3820e0d78a7ed93511ee41d26a53b195d99a
RCristyan <rcristyan9@gmail.com>
2022-07-21 20:18:05 -0300

    Implemented GraphBox asset styling

--- frontend/src/components/GraphBox/index.js
+++ frontend/src/components/GraphBox/index.js
@@ -2,7 +2,20 @@ import './index.css'
 
 function GraphBox() {
     return(
-        <div id='graph-box'></div>
+        <div id='graph-box'>
+            <div id='single-graph-box'>
+                <p id='graph-header'>Umidade do solo</p>
+                <div id='graph-position'></div>
+            </div>
+            <div id='single-graph-box'>
+                <p id='graph-header'>Umidade do ar</p>
+                <div id='graph-position'></div>
+            </div>
+            <div id='single-graph-box'>
+                <p id='graph-header'>Temperatura do ar</p>
+                <div id='graph-position'></div>
+            </div>
+        </div>
     )
 }
 
