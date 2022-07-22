0e16572c407118dce08bb22439d358ab8253edd7
marcosgtavares <marcosgabriel.ro@hotmail.com>
2022-07-22 14:58:51 -0300

    added bot start route

--- backend/api/src/main.py
+++ backend/api/src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from api.src.routers import sensors, trajectory
+from api.src.routers import sensors, trajectory, bot
 
 app = FastAPI(debug=True)
 
 app.include_router(sensors.router)
 app.include_router(trajectory.router)
+app.include_router(bot.router)
 
 @app.get("/")
 def root():
