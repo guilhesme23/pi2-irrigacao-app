561497bd6009e24fdcf66979ab35b22d6ba5b242
RCristyan <rcristyan9@gmail.com>
2022-07-26 15:56:50 -0300

    Added ControlPanelHeader in Trajectory screen

--- frontend/src/screens/Trajectory/index.js
+++ frontend/src/screens/Trajectory/index.js
@@ -2,13 +2,19 @@ import './index.css'
 import Sidebar from '../../components/Sidebar'
 import AreaPropertiesForm from '../../components/AreaPropertiesForm'
 import AreaDisplay from '../../components/AreaDisplay'
+import ControlPanelHeader from '../../components/ControlPanelHeader'
 
 function Trajectory() {
     return (
         <div id="trajectory">
             <Sidebar />
-            <AreaPropertiesForm />
-            <AreaDisplay />
+                <div id="trajectory-screen">
+                    <ControlPanelHeader />
+                    <div id="area-info-box">
+                        <AreaPropertiesForm />
+                        <AreaDisplay />
+                    </div>
+                </div>
         </div>
     )
 }
