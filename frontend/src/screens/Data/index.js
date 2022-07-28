ab712bce27a6861679544dacf61182dc0c6b2b40
RCristyan <rcristyan9@gmail.com>
2022-07-27 23:07:22 -0300

    Added ControlPanelHeader in Data screen

--- frontend/src/screens/Data/index.js
+++ frontend/src/screens/Data/index.js
@@ -1,14 +1,18 @@
 import './index.css'
 import Sidebar from "../../components/Sidebar"
-import DataBox from "../../components/DataBox"
 import GraphBox from '../../components/GraphBox'
+import ControlPanelHeader from '../../components/ControlPanelHeader'
 
 function Data() {
     return(
         <div id="data">
             <Sidebar />
-            <DataBox />
-            <GraphBox />
+            <div id="data-screen">
+                <ControlPanelHeader />
+                <div id="data-info-box">
+                    <GraphBox />
+                </div>
+            </div>
         </div>
     )
 }
