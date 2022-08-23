f506a9045d5302f49db29edbaccf3637423143df
RCristyan <rcristyan9@gmail.com>
2022-08-22 22:42:04 -0300

    Adds charts basic structure

--- frontend/src/assets/SingleGraphBox/index.js
+++ frontend/src/assets/SingleGraphBox/index.js
@@ -1,11 +1,62 @@
 import React from 'react';
 import './index.css'
+import {
+    Chart,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 function SingleGraphBox(props){
+
+    Chart.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend
+    )
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Exemplo'
+            },
+        },
+    };
+
+    const labels = ['A', 'B', 'C']
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [1, 2, 3],
+                borderColor: 'rgb(255, 0, 0)',
+                backgroundColor: 'rgb(123, 123, 123)',
+            },
+        ]
+    }
+
     return(
         <div id='single-graph-box'>
             <p id='graph-header'>{props.headerText}</p>
-            <div id='graph-position'></div>
+            <div id='graph-position'>
+                <Line options={options} data={data}/>
+            </div>
         </div>
     )
 }
