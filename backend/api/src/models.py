8515dc71aa35ceceeebac1391a1b35ac2a967093
Guilherme Marques <marquesguilherme3@gmail.com>
2022-08-24 10:52:41 -0300

    Refactor models

--- backend/api/src/models.py
+++ backend/api/src/models.py
@@ -1,9 +1,12 @@
-from sqlalchemy import Column, ForeignKey, Integer, Float, JSON, TIMESTAMP, Enum
+from sqlalchemy import Column, ForeignKey, Integer, Float, TIMESTAMP, Enum, DateTime
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.mutable import MutableDict
 import enum
 from api.src.database import Base
 
-class sensors(Base):
+class Sensor(Base):
     __tablename__ = "sensors"
 
     id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
@@ -11,35 +14,45 @@ class sensors(Base):
     temp_soil = Column(Float(precision = 6, decimal_return_scale = None), nullable = False)
     humidity_air = Column(Float(precision = 6, decimal_return_scale = None), nullable = False)
     humidity_soil = Column(Float(precision = 6, decimal_return_scale = None), nullable = False)
-    recorded_on = Column(TIMESTAMP, nullable = False)
+    recorded_on = Column(DateTime(timezone=True), server_default=func.now())
 
 class reports_enum(enum.Enum):
     INIT_CYCLE = 'INIT_CYCLE'
     END_CYCLE = 'END_CYCLE'
     LOW_BATTERY = 'LOW_BATTERY'
 
-class reports(Base):
+class Report(Base):
     __tablename__ = "reports"
     id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
     battery_level = Column(Float(precision = 6, decimal_return_scale = None), nullable = False)
-    remaining_points = Column(JSON, nullable = False)
-    recorded_on = Column(TIMESTAMP, nullable = False)
+    remaining_points = Column(MutableDict.as_mutable(JSONB), nullable = False)
+    recorded_on = Column(DateTime(timezone=True), server_default=func.now())
     status_report = Column(Enum(reports_enum), nullable = False)
 
-class field(Base):
-    __tablename__ = "field"
+class Field(Base):
+    __tablename__ = "fields"
 
     id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
     field_width = Column(Float(precision = 6, decimal_return_scale = None), nullable = False)
     field_lenth = Column(Float(precision = 6, decimal_return_scale = None), nullable = False)
-    route = relationship("Child")
+    routes = relationship("Route")
 
-class route(Base):
-    __tablename__ = "route"
+class Route(Base):
+    __tablename__ = "routes"
 
     id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
     base_pos_x = Column(Integer, nullable = False)
     base_pos_y = Column(Integer, nullable = False)
-    irrigation_route = Column(JSON, nullable = False)
+    irrigation_route = Column(MutableDict.as_mutable(JSONB), nullable=False)
     field_id = Column(Integer, ForeignKey("field.id"), nullable = False)
-    
+
+class irrigation_enum(enum.Enum):
+    START = 'START'
+    STOP = 'STOP'
+
+class Irrigation(Base):
+    __tablename__ = 'irrigation_commands'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    irrigate = Column(Enum(irrigation_enum), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
