ae21c6f2ea05634e7381f702ec21acbc20016d5d
RCristyan <rcristyan9@gmail.com>
2022-08-29 10:57:18 -0300

    Implements initial front-back integration

--- frontend/src/components/GraphBox/index.js
+++ frontend/src/components/GraphBox/index.js
@@ -1,34 +1,61 @@
+import { useEffect, useState } from 'react';
 import SingleGraphBox from '../../assets/SingleGraphBox';
 import './index.css'
+import api from '../../services/api';
 
 function GraphBox() {
+    const [graphsData, setGraphsData] = useState({})
+
+    useEffect(() => {
+        api.get("/sensors", {
+            headers: {"Access-Control-Allow-Origin": "*"}
+        }).then(response => {
+            console.log('resposta => ' + response)
+            const data = response.data;
+            setGraphsData(data)
+        })
+    }, [])
+
+    const umidade_solo = graphsData[0] && Object.values(graphsData[0])[3]
+    const umidade_ar = graphsData[0] && Object.values(graphsData[0])[2]
+    const temperatura_ar = graphsData[0] && Object.values(graphsData[0])[0]
+    const temperatura_solo = graphsData[0] && Object.values(graphsData[0])[1]
+
     return(
         <div id='graph-box'>
             <div id='graph-box-title'>
                 <p>Relatório dos sensores</p>
             </div>
-            <div id='graph-box-items'>
+            { graphsData && <div id='graph-box-items'>
                 <SingleGraphBox 
                     headerText='Umidade do solo (%)'
                     label = 'Umidade'
-                    values = {[20, 18, 19, 20, 20]}
+                    values = {[
+                        umidade_solo
+                    ]}
                 />
                 <SingleGraphBox 
                     headerText='Umidade do ar (%)'
                     label = 'Umidade'
-                    values = {[35, 33.5, 35, 34, 35]}
+                    values = {[
+                        umidade_ar
+                    ]}
                 />
                 <SingleGraphBox 
                     headerText = 'Temperatura do ar (°C)'
                     label = 'Temperatura'
-                    values = {[28.5, 29, 27.5, 26, 27]}
+                    values = {[
+                        temperatura_ar
+                    ]}
                 />
                 <SingleGraphBox 
                     headerText = 'Temperatura do solo (°C)'
                     label = 'Temperatura'
-                    values = {[22, 22.5, 22, 21, 21]}
+                    values = {[
+                        temperatura_solo
+                    ]}
                 />
-            </div>
+            </div>}
         </div>
     )
 }
