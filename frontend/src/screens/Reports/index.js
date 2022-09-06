eab6a9b376463c58869a69502cdf0c4a1982f281
RCristyan <rcristyan9@gmail.com>
2022-09-06 20:50:07 -0300

    Implements integration with waterbalance api

--- frontend/src/screens/Reports/index.js
+++ frontend/src/screens/Reports/index.js
@@ -3,8 +3,33 @@ import Sidebar from '../../components/Sidebar';
 import SingleGraphBox from '../../assets/SingleGraphBox';
 import SensorStatusBox from '../../assets/SensorStatusBox';
 import './index.css'
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 function Reports(){
+
+    const [graphDates, setGraphsDates] = useState(0)
+    const [rainValue, setRainValue] = useState(0)
+    const [deficit, setDeficit] = useState(0)
+
+    useEffect(() => {
+        async function fecthWaterbalanceData() {
+            await api.get('/waterbalance', {
+                headers: {"Access-Control-Allow-Origin": "*"}
+            }).then(response => {
+                const waterbalanceData = response.data
+
+                setGraphsDates(waterbalanceData.map(object => object.data).slice(24,31))
+                setRainValue(waterbalanceData.map(object => object.precipitacao).slice(24,31))
+                setDeficit(waterbalanceData.map(object => object.deficit).slice(24,31))
+            })
+        }
+
+        fecthWaterbalanceData()
+    }, [])
+
+    console.log(graphDates)
+
     return(
         <div id="reports">
             <Sidebar />
@@ -13,24 +38,28 @@ function Reports(){
                 <div id="reports-info-box">
                     <div id='reports-info-box-graph-items'>
                         <SingleGraphBox 
-                            headerText = 'Chuva + irrigação'
-                            label = 'Chuva + irrigação'
-                            values = {[80, 82, 90, 85, 86]}
+                            headerText = 'Chuva'
+                            label = 'Chuva'
+                            values = {rainValue}
+                            x_values = {graphDates}
                         />
                         <SingleGraphBox 
                             headerText = 'Deficit hídrico'
                             label = 'Deficit hídrico'
-                            values = {[80, 82, 90, 85, 86]}
+                            values = {deficit}
+                            x_values = {graphDates}
                         />
                         <SingleGraphBox 
                             headerText = 'Reposição'
                             label = 'Reposição'
-                            values = {[75, 77, 79, 83, 86]}
+                            values = {[]}
+                            x_values = {graphDates}
                         />
                         <SingleGraphBox 
                             headerText = 'Retirada'
                             label = 'Retirada'
-                            values = {[88, 90, 85, 83, 80]}    
+                            values = {[]}
+                            x_values = {graphDates}
                         />
                     </div>
                     <SensorStatusBox />
